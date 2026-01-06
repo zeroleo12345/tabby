95a1dfa77a47ea831a4d065edb75ab50cbf5e97a
zeroleo12345 <zeroleo12345@163.com>
2026-01-07 00:03:50 +0800

    add

--- tabby-terminal/src/frontends/xtermFrontend.ts
+++ tabby-terminal/src/frontends/xtermFrontend.ts
@@ -177,6 +177,7 @@ export class XTermFrontend extends Frontend {
             console.log("1111 keyboard from xterm.js:", event)
 
             if (this.hostApp.platform !== Platform.Web) {
+                console.log("1111 ", event.getModifierState('Alt'), event.key.toLowerCase() === ' ')
                 if ( event.getModifierState('Alt') && event.key.toLowerCase() === ' ' ) {
                     event.preventDefault()
                     console.log('捕获 Alt+Space 快捷键，已阻止系统默认行为')
