f8173a2579a47605f697d5e8acbd5518009b068c
zeroleo12345 <zeroleo12345@163.com>
2026-02-06 08:17:25 +0800

    fix

--- tabby-settings/src/services/configSync.service.ts
+++ tabby-settings/src/services/configSync.service.ts
@@ -103,7 +103,7 @@ export class ConfigSyncService {
             // }
             const digest = this.hashContent(localContent)
             console.log(`111 upload local config, remote digest: ${digest}, local digest: ${this.lastRemoteChange.digest}`)
-            console.log(`111 upload content:`, localContent)
+            console.log(`111 content upload:`, localContent)
             if (this.lastRemoteChange.digest === digest) {
                 this.logger.info('Config unchanged, skipping upload')
                 return
@@ -177,7 +177,7 @@ export class ConfigSyncService {
         this.lastRemoteChange.modified_at = new Date(remoteConfig.modified_at)
         const digest = this.hashContent(remoteConfig.content)
         console.log(`111 sync remote config, remote digest: ${digest}, local digest: ${this.lastRemoteChange.digest}`)
-        console.log(`111 downloaded content:`, remoteConfig.content)
+        console.log(`111 content download:`, remoteConfig.content)
         if (this.lastRemoteChange.digest === digest) {
             this.logger.info('Config unchanged, skipping sync')
             return
