04a6dd6add58bfe78545c486bf0dac50e9a100b3
zeroleo12345 <zeroleo12345@163.com>
2026-02-07 20:32:44 +0800

    fix

--- tabby-core/src/services/hotkeys.service.ts
+++ tabby-core/src/services/hotkeys.service.ts
@@ -292,15 +292,9 @@ export class HotkeysService {
             this.clearCurrentKeystrokes()
         }
         // console.log(`111 matched`)
-        if (['select-all'].includes(matches[0].id)) {
-            console.log(`111 hotkey select-all`)
-            if (this.contextKey['terminalTabFocus'] === false) {
-                console.log(`select-all: but no focus on terminal tab`)
-                // 当 terminal tab focus 时候, select-all 快捷键才生效, 否则 return null
-                return null
-            } else {
-                console.log(`select-all matched`)
-            }
+        if (['select-all'].includes(matches[0].id) && this.contextKey['terminalTabFocus'] === false) {
+            // 当 terminal tab focus 时候, select-all 快捷键才生效, 否则 return null
+            return null
         }
         return matches[0].id
     }
