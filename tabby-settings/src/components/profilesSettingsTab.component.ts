145123ba47ae52c95ce61cdd2575cf4f30cfdd6f
zeroleo12345 <zeroleo12345@163.com>
2026-02-14 23:14:53 +0800

    fix: new profile

--- tabby-settings/src/components/profilesSettingsTab.component.ts
+++ tabby-settings/src/components/profilesSettingsTab.component.ts
@@ -120,7 +120,7 @@ export class ProfilesSettingsTabComponent extends BaseComponent {
         if (!provider) {
             throw new Error('Cannot edit a profile without a provider')
         }
-        modal.componentInstance.profile = deepClone(profile)
+        modal.componentInstance._profile = deepClone(profile)
         modal.componentInstance.profileProvider = provider
 
         const result = await modal.result.catch(() => null)
