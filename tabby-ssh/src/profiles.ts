759225f777b79f99b4e01f85501076e8d2e57a61
Lyn <13072815+zeroleo12345@users.noreply.github.com>
2026-08-06 10:31:01 +0800

    feat: add toggle for Huawei legacy erase and arrow (#71)

--- tabby-ssh/src/profiles.ts
+++ tabby-ssh/src/profiles.ts
@@ -44,6 +44,7 @@ export class SSHProfilesService extends QuickConnectProfileProvider<SSHProfile> 
             httpProxyPort: null,
             reuseSession: true,
             input: { backspace: 'backspace' },
+            enableLegacyCursorSequenceFix: false,
         },
         clearServiceMessagesOnConnect: true,
     }
